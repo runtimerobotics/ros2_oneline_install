528b554b49a9fb66b7ebd4d26e236751b9ffdc9a
therobocademy <therobocademy@gmail.com>
2024-03-07 18:06:20 +0530

    Updated ros 2 iron scripts

--- ros2_uninstall_iron.sh
+++ ros2_uninstall_iron.sh
@@ -7,17 +7,17 @@
 
 echo "################################################################"
 echo ""
-echo ">>> {Uninstalling ROS Humble Installation from your computer}"
+echo ">>> {Uninstalling ROS Iron Installation from your computer}"
 echo ""
 echo ">>> {It will take around few minutes to complete}"
 echo ""
-sudo apt-get purge ros-humble-*
+sudo apt-get purge ros-iron-*
 echo ""
 echo "#################################################################"
 echo ""
 echo ">>> {Auto removing dependent packages}"
 sudo apt-get autoremove
 echo ""
-echo ">>> {Done: ROS Humble Uninstall}"
+echo ">>> {Done: ROS Iron Uninstall}"
 echo "#################################################################"
 
