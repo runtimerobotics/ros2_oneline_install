a1a9233dd3e4f15c6296e176e19ab533694d466d
runtimerobotics <runtimerobotics@gmail.com>
2025-06-06 02:27:19 +0530

    Updated script to add new signature

--- ros2_install_jazzy.sh
+++ ros2_install_jazzy.sh
@@ -86,7 +86,13 @@ echo ">>> {Installing ROS 2 APT Source package}"
 
 
 sudo apt update 
-sudo apt install -y curl openssl ros2-apt-source
+sudo apt install -y curl openssl 
+
+
+export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F\" '{print $4}')
+curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo $VERSION_CODENAME)_all.deb" # If using Ubuntu derivates use $UBUNTU_CODENAME
+sudo apt install /tmp/ros2-apt-source.deb
+
 
 #fi
 
